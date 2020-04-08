adf7a2afede8fc5f72f05be7fad46d17df771581
Oleksii-github <oleksii.barash@gmail.com>
2020-04-07 19:51:27 -0700

    Cached matrix function

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-ergerer
+        m <- NULL # re-initiate matrix
+        set <- function(y) { # set matrix
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x # get matrix
+        setmean <- function(solve) m <<- solve
+        getmean <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
@@ -12,4 +22,14 @@ ergerer
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getsolve()
+        if(!is.null(m)) { # check if the is cached matrix
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m) # get inverse of matrix
+        m
 }
